7957dfb222933ceb3d146f48519e57076c7d20b8
Vladimir Salin <salinvs@gmail.com>
2020-06-19 16:38:06 +0100

    send back the response

--- services/senderService.js
+++ services/senderService.js
@@ -1,6 +1,22 @@
+const axios = require('axios')
+
+const PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN
+
 module.exports = {
   sendResponse: (senderPsid, response) => {
-    console.log("sender API call")
-    console.log(response)
+    const requestBody = {
+      "recipient": {
+        "id": senderPsid
+      },
+      "message": response
+    }
+
+    axios.post('https://graph.facebook.com/v2.6/me/messages', requestBody, {
+      params: { "access_token": PAGE_ACCESS_TOKEN }
+    }).then(() => {
+      console.log('message sent!')
+    }).catch((err) => {
+      console.error("Unable to send message:" + err);
+    })
   }
 }
